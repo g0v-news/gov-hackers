b2c9c7d688dabe31883aeea24f1117b68fb1291c
chihao <chihaoyo@gmail.com>
2017-03-22 22:09:15 +0800

    Add fonts.com webfont (but failed)

--- src/assets/fonts.js
+++ src/assets/fonts.js
@@ -1,4 +1,4 @@
 export default {
-  serif: '"Fantasque Sans Mono", "Helvetica Neue", "PingFang TC", "微軟正黑體", serif',
+  serif: '"Fantasque Sans Mono", "Helvetica Neue", "M Sung HK W42", "PingFang TC", "微軟正黑體", serif',
   body: '"Fantasque Sans Mono", "Helvetica Neue", "PingFang TC", "微軟正黑體", sans-serif',
 };
